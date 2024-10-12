35e342be976dd18fd165a2ea0cf9ff06f23fe3b5
Norihiro Kamae <norihiro@nagater.net>
2024-10-12 12:16:15 +0900

    encoder-proc: Align memory allocation addresses

--- encoder-proc/util.c
+++ encoder-proc/util.c
@@ -1,17 +1,32 @@
 #include <stdlib.h>
 #include <util/bmem.h>
 
+#define ALIGNMENT 32
+
 void *bmalloc(size_t size)
 {
+#ifdef _WIN32
+	return _aligned_malloc(size, ALIGNMENT);
+#else
+	// TODO: align
 	return malloc(size);
+#endif
 }
 
 void *brealloc(void *ptr, size_t size)
 {
+#ifdef _WIN32
+	return _aligned_realloc(ptr, size, ALIGNMENT);
+#else
 	return realloc(ptr, size);
+#endif
 }
 
 void bfree(void *ptr)
 {
+#ifdef _WIN32
+	_aligned_free(ptr);
+#else
 	free(ptr);
+#endif
 }
